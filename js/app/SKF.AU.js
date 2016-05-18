4ae862042e40dc6c77ecb8191d698f23d84d45ed
SlavaFedor <slavafedor@gmail.com>
2016-05-17 22:19:31 -0600

    Adding main object, restructuring

---
+++ js/app/SKF.AU.js
@@ -0,0 +1,18 @@
+var SKF = {};
+SKF.AU = {
+	Version : "0.0.1",
+	Name : "AdviserU",
+	Author : "SKF",
+	
+};
+
+Object.defineProperty(SKF.AU, "IMPORTANCE",
+ {
+		Info : 0,
+		NotImportant : 1,
+		SomewhatImportant : 2,
+		Important : 3,
+		VeryImportant : 4,
+		Critical : 5
+	}
+);
